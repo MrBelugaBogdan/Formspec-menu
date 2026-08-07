3735e57ecadd5b3daf51cc5229ca647e6d124a65
MrBelugaBogdan <medvedovbogdan19@gmail.com>
2026-08-07 18:18:06 +0300

    Refactor config.js by cleaning up comments and code
    Removed unused comments and simplified the generateGivemeCommand function.

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
-// Хеш паролю доступу
+// 🔑 MD5-хеш вашого пароля для входу (зараз стоїть хеш від "123456")
 const PASSPHRASE_HASH = "9096095f6d674a667399b067ee3df916";
 
-// Розмір сітки (1 unit у Formspec = 40px)
-const GRID_SCALE = 40;
-
-// Шаблон команди /giveme
+// Функція обгортки для генерації команди /giveme (якщо потрібна)
 function generateGivemeCommand(formspecCode) {
-  return '/giveme books:empty 1 0 "\\u0001owner\\u0002Деси\\u0003description\\u0002Інвентар\\u0003page\\u0001\\u0003page_max\\u0001\\u0003text_b64\\u00020LPRgNCw0L/QvtC9IDIgLSDRhNC20LXRgNCwINC/0L7Qu9GMINC70ZbQstC90LAK\\u0003title_b64\\u00020JrRgNC40YHQuyBCYWc=\\u0003formspec\\u0002' + formspecCode + '\\u0003"';
+    return formspecCode; // Можеш змінити формат видачі сюди
 }
